81f0ee9a0a95afc49d0bb3e4cd1dfe7ca081e436
th1sman <jaim.quilodrandev@gmail.com>
2023-12-27 20:16:40 -0300

    Display de productos fixeado

--- water-front-next/app/page.js
+++ water-front-next/app/page.js
@@ -6,7 +6,7 @@ const Home = () => {
       <h1 className="leading-relaxed font-extrabold text-4xl text-center mt-4 py-2 sm:py-4 text-black">
         Agua San Jose
       </h1>
-      <div className="gap-y-8">
+      <div className="grid ">
         <Products />
       </div>
     </div>
